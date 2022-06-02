45149e8ed504d54b8b095a410de51915429ae7a9
Aleh Tsviatsinski <atsviatsinski@exadel.com>
2022-06-02 17:49:09 +0300

    Initial project create

---
+++ Evalutor/Evaluator.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace Evaluator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Theory]
+        [InlineData("")]
+        public void Test2(string expression)
+        {
+
+        }
+    }
+}
